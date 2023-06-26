823897cfd8267543a1ceb1c660369ad57f1dec08
“Vuong <vuong.tran@rikai.technology>
2023-06-26 13:59:39 +0700

    config dashboard

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,14 +1,14 @@
 class Admin::InvoicesController < ApplicationController
     def revenue
-        if params[:search_month].present?
-          year, month = params[:search_month].split("-")
-          @invoices = Invoice.includes(:user).where("EXTRACT(MONTH FROM created_at) = ? AND EXTRACT(YEAR FROM created_at) = ?", month.to_i, year.to_i)
-        else
-          @invoices = Invoice.includes(:user)
-        end
-    
-        @total_revenue = calculate_total_revenue(@invoices)
+      if params[:search_month].present?
+        year, month = params[:search_month].split("-")
+        @invoices = Invoice.includes(:user).where("EXTRACT(MONTH FROM created_at) = ? AND EXTRACT(YEAR FROM created_at) = ?", month.to_i, year.to_i)
+      else
+        @invoices = Invoice.includes(:user)
       end
+  
+      @total_revenue = calculate_total_revenue(@invoices)
+    end
 
       private
 
